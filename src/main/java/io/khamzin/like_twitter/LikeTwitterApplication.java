96e5d0526e1dbb1eabfa249512039e7464b99d44
thebestWasabi <openpw@yandex.ru>
2023-06-10 15:07:13 +0300

    Загрузил начальную конфигурацию

---
+++ src/main/java/io/khamzin/like_twitter/LikeTwitterApplication.java
@@ -0,0 +1,13 @@
+package io.khamzin.like_twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeTwitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeTwitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/khamzin/like_twitter/LikeTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.khamzin.like_twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeTwitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
